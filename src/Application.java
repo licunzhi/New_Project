9fc5f5ca628f8b6ea429292911055c13bbb98f18
licunzhi <2718272293@qq.com>
2019-03-03 10:11:42 +0800

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,225 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @ClassName Application
+ * @Description 功能描述
+ * @Author lcz
+ * @Date 2019/03/02 19:52
+ */
+public class Application {
+
+    // 代码中常量设置位置
+
+    // K值设置
+    private static final int K = 10;
+//    private static final int K = 20;
+//    private static final int K = 30;
+//    private static final int K = 40;
+//    private static final int K = 50;
+//    private static final int K = 60;
+//    private static final int K = 70;
+//    private static final int K = 80;
+//    private static final int K = 90;
+
+    public static void main(String[] args) {
+
+        // base数据集信息
+        int[][] base = new ReadFile().readFile("u1.base", 943, 1682);
+
+        // test数据集
+        int[][] test = new ReadFile().readFile("u1.test", 943, 1682);
+
+        // 用户相似度矩阵
+        double[][] similarityMatrix = Application.produceSimilarityMatrix(base);
+
+        // 分数预测结果
+        double[][] matrix = predictScore(base, similarityMatrix);
+
+        System.out.println(">>>>>>>>>>>>>>>>>>>>开始进行召回率计算>>>>>>>>>>>>>>>>>>>>>>>>>");
+        int top_zhaohui = 0;
+        int buttom_zhaohui = 0;
+        int buttom_zhunque = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] != 0 && test[i][j] != 0) {
+                    top_zhaohui++;
+                }
+                if (test[i][j] != 0) {
+                    buttom_zhaohui++;
+                }
+                if (matrix[i][j] != 0) {
+                    buttom_zhunque++;
+                }
+            }
+        }
+        System.out.println("召回率计算结果：" + top_zhaohui/ 1.0 / buttom_zhaohui);
+    }
+
+
+
+    // 预测评分
+    private static double[][] predictScore(int[][] base, double[][] similarityMatrix) {
+
+        double[][] matrix = new double[943][1682];// 预测的分数结果矩阵 （用户index 预测分数）
+
+
+        for (int i = 0; i < base.length; i++) {
+            System.out.println("总数:" + base.length + ",当前结算位置:" + (i + 1));
+            for (int j = 0; j < base[i].length; j++) {
+
+                // 未进行评分,需要进行预测的项目
+                if (base[i][j] == 0) {
+
+                    // 查找K近邻信息结果返回 用户index 相似度数据value
+                    Map<Integer, Double> nebers = findNeBers(base, similarityMatrix, i, j);
+                    //System.out.println(nebers.size());
+
+                    double similaritySum = 0;// 分母  比如 0.5 + 0.6
+                    double sum = 0;// 分子
+                    double score = 0;
+                    for (Map.Entry<Integer, Double> entry : nebers.entrySet()) {
+                        sum += similarityMatrix[i][entry.getKey()]* base[entry.getKey()][j];//预测分数的算法：不同的相关系数*同一用户的不同电影的分数之和
+                        similaritySum += similarityMatrix[i][entry.getKey()];//相似度值和
+                    }
+
+                    // 预测分数计算
+                    if (similaritySum == 0)// 排除分母为零的情况
+                        score = 0;
+                    else
+                        score = sum / similaritySum;
+
+                    matrix[i][j] = score;
+                }
+
+            }
+        }
+        return matrix;
+    }
+
+    private static Map<Integer, Double> findNeBers(int[][] base, double[][] similarityMatrix, int i, int j) {
+        Map<Integer, Double> neberMap = new HashMap<>();
+
+        // 查找K近邻的方法
+        for (int u = 0; u < 943; u++) {
+            if (base[u][j] != 0) { // 用户u对物品j存在评分
+                neberMap.put(u, similarityMatrix[i][u]);
+            } else {
+                neberMap.put(u, 0.0);
+            }
+        }
+        // 相似度的存储数据格式为：用户index 用户相似度value
+        Map<Integer, Double> result = sortByValueDescending(neberMap);
+
+        Map<Integer, Double> final_result = new HashMap<>();
+        int k_value = 0;
+        for (Map.Entry<Integer, Double> entry : result.entrySet()) {
+            if (entry.getValue() != 0) {
+                final_result.put(entry.getKey(), entry.getValue());
+                k_value++;
+            }
+            if (entry.getValue() == 0) {
+                break;
+            }
+            if (k_value >= K) {
+                break;
+            }
+        }
+        return final_result;
+    }
+
+    //降序排序map中的value数据
+    private static <K, V extends Comparable<? super V>> Map<K, V> sortByValueDescending(Map<K, V> map)
+    {
+        List<Map.Entry<K, V>> list = new LinkedList<>(map.entrySet());
+        list.sort((o1, o2) -> {
+            int compare = (o1.getValue()).compareTo(o2.getValue());
+            return -compare;
+        });
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+
+
+
+
+
+
+    // 计算相似度
+    private static double[][] produceSimilarityMatrix(int[][] base) {
+        double[][] similarityMatrix = new double[943][943];
+
+        for (int i = 0; i < 943; i++) {
+            for (int j = 0; j < 943; j++) {
+                // 用户自己和自己相似度为1
+                if (i == j) {
+                    similarityMatrix[i][j] = 1;
+                }
+                // 计算和其他用户相似度
+                else {
+                    similarityMatrix[i][j] = Application.computeSimilarity(base[i], base[j]);
+                }
+            }
+        }
+        return similarityMatrix;
+    }
+
+    // 计算相似度具体方法
+    private static double computeSimilarity(int[] item1, int[] item2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        int j = 0;
+        for (int i = 0; i < item1.length; i++) {
+            if(item1[i] != 0 && item2[i] !=0) {
+                list1.add(item1[i]);
+                list2.add(item2[i]);
+            }
+            j++;
+        }
+        //研究计算相似度的方法具体的实现   返回的是相似度的参数
+        return Application.pearsonCorrelation(list1,list2);
+    }
+
+    private static double pearsonCorrelation(List<Integer> a, List<Integer> b) {
+        int num = a.size();//集合a作为参照集  用户i
+        int sum_prefOne = 0;
+        int sum_prefTwo = 0;
+        int sum_squareOne = 0;
+        int sum_squareTwo = 0;
+        int sum_product = 0;
+        for (int i = 0; i < num; i++) {
+            sum_prefOne += a.get(i);//∑xi
+            sum_prefTwo += b.get(i);//∑yi
+            sum_squareOne += Math.pow(a.get(i), 2);//∑xi2
+            sum_squareTwo += Math.pow(b.get(i), 2);//∑yi2
+            sum_product += a.get(i) * b.get(i);//∑xiyi
+        }
+        double sum = num * sum_product - sum_prefOne * sum_prefTwo;//n∑xiyi-∑xi∑yi
+        double den = Math.sqrt((num * sum_squareOne - Math.pow(sum_squareOne, 2)) * (num * sum_squareTwo - Math.pow(sum_squareTwo, 2)));
+        //√((n∑xi-(∑xi2)2)*(n∑yi-(∑yi2)2))------这就是皮尔逊相关系数
+        double result;
+        if(den==0) result=0;
+        else result = sum / den;
+        //n∑xiyi-∑xi∑yi/√((n∑xi-(∑xi2)2)*(n∑yi-(∑yi2)2))
+        return result;
+    }
+
+
+
+
+
+
+}
---
+++ src/ReadFile.java
@@ -0,0 +1,55 @@
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * 读取测试集代码类
+ *
+ * @author mjc
+ */
+public class ReadFile {
+
+    /**
+     * 读取文件数据的方法
+     *
+     * @param fileName ： 文件的名字
+     * @return ： 返回值的是二维数组
+     */
+    public int[][] readFile(String fileName, int arr_x, int arr_y) {
+
+        int[][] user_movie = new int[arr_x][arr_y];//这些蓝色是定义在 Base类中的常量   代表的是具体的数字   创建二位数组存储数据
+
+        try {
+            File file = new File(fileName);
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line = "";
+            int i = 0;
+            while (br.ready()) {
+                line = br.readLine();//每次读取数据集中的一行数据   readline
+                String[] data = line.split("\t");/*用 tab 号分隔数字*/
+                int[] ddd = new int[4];
+                for (int j = 0; j < data.length; j++) {
+                    ddd[j] = Integer.parseInt(data[j]);
+                }
+                //读取的数据进行分割成为四部分：1	1	5	874965758
+                user_movie[ddd[0] - 1][ddd[1] - 1] = ddd[2];//存储到具体的二位数组之中
+                i++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*return preference;*/
+        return user_movie;
+
+    }
+
+}
+
+
+
